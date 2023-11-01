91b3d51180e3060393ee6e2e94bbbd54db323372
DaviMatheus <matdavimat@gmail.com>
2023-11-01 01:30:30 -0300

    🐴(#173) starting_gateway_logic
    Co-authored-by: Natanael Filho <filhonatanael01@gmail.com>

--- src/gateway.js
+++ src/gateway.js
@@ -1,16 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+import helmet from 'helmet';
+import httProxy from 'express-http-proxy'
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
+app.use(logger('start'))
+app.use(helmet())
+app.use(express.urlencoded({extended: true}))
 
+app.get('/', (req, res)  => {
+    return res.json({ message: 'Running Gateway' })
+})
+
+
+app.use('/user', httProxy('localhost:8000', {timeout: 3000}))
 const PORT = process.env.PORT || 4000;
 
 const gateway = app.listen(PORT, () => {
     console.log(`Gateway is running on ${PORT}`);
 });
 
-module.exports = { gateway, app };
+export { gateway, app };
