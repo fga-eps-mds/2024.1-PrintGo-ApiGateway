eab959fccf340e59edc81c829735ea565034aed3
DaviMatheus <matdavimat@gmail.com>
2023-11-01 01:49:54 -0300

    🐴(#173) adding_automatization_logic
    Co-authored-by: fernandes-natanael <filhonatanael01@gmail.com>

--- src/gateway.js
+++ src/gateway.js
@@ -3,8 +3,14 @@ import cors from 'cors';
 import logger from 'morgan';
 import helmet from 'helmet';
 import httProxy from 'express-http-proxy'
-
+import { resolve } from 'path'
+import { readFileSync } from 'fs'
+import { load } from 'js-yaml'
 const app = express();
+
+const pathFile = resolve(process.cwd(), 'config.yml')
+const  readConfig = readFileSync(pathFile, {encoding: 'utf8'})
+const { services } = load(readConfig, {json: true})
 
 app.use(express.json());
 
@@ -17,8 +23,11 @@ app.get('/', (req, res)  => {
     return res.json({ message: 'Running Gateway' })
 })
 
+services.forEach(({ name, url }) => {
+    app.use(`/${name}`, httProxy(url, {timeout: 3000}))
+ 
+});
 
-app.use('/user', httProxy('localhost:8000', {timeout: 3000}))
 const PORT = process.env.PORT || 4000;
 
 const gateway = app.listen(PORT, () => {
