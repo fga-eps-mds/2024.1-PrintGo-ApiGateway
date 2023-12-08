f9b009debbb1016be00377ca89a4a1ac9a268eb0
Natanael Fernandes <filhonatanael01@gmail.com>
2023-12-08 12:49:26 -0300

    atualizando api gateway para usar variaveis de ambiente

--- src/gateway.ts
+++ src/gateway.ts
@@ -2,45 +2,26 @@ import express from 'express';
 import cors from 'cors';
 import logger from 'morgan';
 import helmet from 'helmet';
-import httProxy from 'express-http-proxy'
-import { resolve } from 'path'
-import { readFileSync } from 'fs'
-import { load } from 'js-yaml'
-import 'dotenv/config'
+import httProxy from 'express-http-proxy';
+import 'dotenv/config';
 
 const app = express();
 
-const pathFile = resolve(process.cwd(), process.env.CONFIG_FILE || 'config.yml')
-const  readConfig = readFileSync(pathFile, {encoding: 'utf8'})
-
-type Service = {
-    name: string;
-    url: string;
-}
-
-type ServicesConfig = {
-    services: Service[];
-}
-
-const servicesConfig  = load(readConfig, {json: true}) as ServicesConfig
-
 app.use(express.json());
-
 app.use(cors());
-app.use(logger('start'))
-app.use(helmet())
-app.use(express.urlencoded({extended: true}))
+app.use(logger('start'));
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res)  => {
-    return res.json({ message: 'Running Gateway' })
-})
-
-servicesConfig.services.forEach(({ name, url }) => {    
-    app.use(`/${name}`, httProxy(url, {timeout: 10000}))
+app.get('/', (req, res) => {
+    return res.json({ message: 'Running Gateway' });
 });
 
-const PORT = process.env.PORT || 4000;
+app.use('/schedular', httProxy(process.env.URL_SCHEDULER_SERVICE, { timeout: 10000 }));
+app.use('/user', httProxy(process.env.URL_USER_SERVICE, { timeout: 10000 }));
+app.use('/printer', httProxy(process.env.URL_PRINTER_SERVICE, { timeout: 10000 }));
 
+const PORT = process.env.PORT || 4000;
 const gateway = app.listen(PORT, () => {
     console.log(`Gateway is running on ${PORT}`);
 });
