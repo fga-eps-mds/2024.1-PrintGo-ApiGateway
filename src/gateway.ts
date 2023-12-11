f72398ac667025807e436acf8d0a27d3953adb3c
Natanael Fernandes <filhonatanael01@gmail.com>
2023-12-10 22:31:14 -0300

    adicionando dot env

--- src/gateway.ts
+++ src/gateway.ts
@@ -17,9 +17,9 @@ app.get('/', (req, res) => {
     return res.json({ message: 'Running Gateway' });
 });
 
-app.use('/schedular', httProxy(process.env.URL_SCHEDULAR_SERVICE, { timeout: 10000 }));
-app.use('/user', httProxy(process.env.URL_USER_SERVICE, { timeout: 10000 }));
-app.use('/printer', httProxy(process.env.URL_PRINTER_SERVICE, { timeout: 10000 }));
+app.use('/schedular', httProxy(process.env.URL_SCHEDULAR_SERVICE ? process.env.URL_SCHEDULAR_SERVICE : 'https://2023-1-schedula-gerenciador-de-localidades.vercel.app', { timeout: 10000 }));
+app.use('/user', httProxy(process.env.URL_USER_SERVICE ? process.env.URL_USER_SERVICE : 'https://2023-2-print-go-user-service-seven.vercel.app', { timeout: 10000 }));
+app.use('/printer', httProxy(process.env.URL_PRINTER_SERVICE ? process.env.URL_PRINTER_SERVICE : 'https://2023-2-print-go-printer-service.vercel.app', { timeout: 10000 }));
 
 const PORT = process.env.PORT || 4000;
 const gateway = app.listen(PORT, () => {
