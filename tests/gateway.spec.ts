533cae6f84a3f8170d530853e3ac3f06d55dc1ee
Natanael Fernandes <filhonatanael01@gmail.com>
2023-12-10 21:35:40 -0300

    aumentando coverage

--- tests/gateway.spec.ts
+++ tests/gateway.spec.ts
@@ -5,9 +5,23 @@ describe('GET /', () => {
     afterAll((done) => {
         gateway.close(done);
     });
+
   it('should return a 200 status and the correct message', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.body.message).toBe('Running Gateway');
+  });
+
+  it('should return a 200 status and the correct message', async () => {
+    const response = await request(app).get('/printer/impressora');
+    expect(response.status).toBe(200);
+  });
+  it('should return a 200 status and the correct message', async () => {
+    const response = await request(app).get('/user');
+    expect(response.status).toBe(200);
+  });
+  it('should return a 200 status and the correct message', async () => {
+    const response = await request(app).get('/schedular');
+    expect(response.status).toBe(200);
   });
 });
