03d853ebbe310a22aaab50293cffc575ee6fdef1
Natanael Fernandes <filhonatanael01@gmail.com>
2023-12-10 22:28:02 -0300

    adicionando dot env

--- tests/gateway.spec.ts
+++ tests/gateway.spec.ts
@@ -3,13 +3,17 @@ import { app, gateway } from '../src/gateway';
 import 'dotenv/config';
 
 describe('GET /', () => {
+    beforeAll(() => {
+      process.env.URL_USER_SERVICE = "https://2023-2-print-go-user-service-seven.vercel.app"
+      process.env.URL_PRINTER_SERVICE = "https://2023-2-print-go-printer-service.vercel.app"
+      process.env.URL_SCHEDULAR_SERVICE = "https://2023-1-schedula-gerenciador-de-localidades.vercel.app"
+      
+    })
     afterAll((done) => {
         gateway.close(done);
     });
 
-  it('should return a 200 status and the correct message', async () => {
-    console.log(process.env.URL_SCHEDULAR_SERVICE);
-    
+  it('should return a 200 status and the correct message', async () => {    
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.body.message).toBe('Running Gateway');
